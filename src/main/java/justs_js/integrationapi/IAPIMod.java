5528277d15b14767d9b75f203eb9d10249d5d8f4
Just <grivten@yandex.ru>
2026-01-08 17:24:58 +0700

    removed unnecessary showcase

--- src/main/java/justs_js/integrationapi/IAPIMod.java
+++ src/main/java/justs_js/integrationapi/IAPIMod.java
@@ -1,6 +1,5 @@
 package justs_js.integrationapi;
 
-import justs_js.integrationapi.impl.twitch.eventsub.TwitchIntegrationImpl;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -9,9 +8,6 @@ import org.slf4j.LoggerFactory;
 public class IAPIMod implements ModInitializer {
 	public static final String MOD_ID = "integrationapi";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	private static TwitchIntegrationImpl twitch;
-
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Integration API loaded.");
